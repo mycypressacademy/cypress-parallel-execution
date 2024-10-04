b8f82d6e033da7ebf6b1f825b067e3a0a787b907
Cypresstraining <cypresstraining7@gmail.com>
2024-10-04 19:38:04 +0530

    added project Id to connect with cypress cloud project

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:"http://cypresstesting7-001-site1.jtempurl.com/",
+    projectId:kwuf95,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
